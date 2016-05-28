69c744e2826d6357a3a678a6582c47c03c41735f
Puneeth Chaganti <punchagan@muse-amuse.in>
2016-05-28 19:58:02 +0530

    Fix SITE_URL
    The RSS feed is broken and this fixes it.

--- blog/conf.py
+++ blog/conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Jaidev Deshpande"  # (translatable)
 BLOG_TITLE = "Broca's Brain"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://jaidevd.github.io/blog/"
+SITE_URL = "https://jaidevd.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "http://localhost:8000/"
