f20b00d59ed5914359b0681fa94f77d72cfb11b2
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2016-08-22 16:25:35 +0530

    Batch learning for backpropagation

--- notes/nnml/ng_backprop.py
+++ notes/nnml/ng_backprop.py
@@ -23,36 +23,36 @@ b1 = np.random.rand(3, 1)
 b2 = np.random.rand(2, 1)
 losses = []
 
-alpha = 0.3
+alpha = 0.5
 
-for i in range(100):
 
-    for j in range(X.shape[0]):
-        sample = X[j, :]
-        target = y[j, :]
+def run(w1, w2, b1, b2):
+    for i in range(1000000):
 
-        a1 = sample.reshape(-1, 1)
+        a1 = X.T
         z2 = np.dot(w1, a1) + b1
         a2 = sigmoid(z2)
         z3 = np.dot(w2, a2) + b2
         a3 = sigmoid(z3)
 
-        loss = ((a3 - target) ** 2).sum()
+        loss = ((a3 - y.T) ** 2).sum()
         print loss
         losses.append(loss)
 
-        target = target.reshape(-1, 1)
-        del3 = - (target - a3) * a3 * (1 - a3)
+        del3 = - (y.T - a3) * a3 * (1 - a3)
         del2 = np.dot(w2.T, del3) * a2 * (1 - a2)
         gradw2 = np.dot(del3, a2.T)
-        gradb2 = del3
+        gradb2 = del3.sum(1).reshape(-1, 1)
         gradw1 = np.dot(del2, a1.T)
-        gradb1 = del2
+        gradb1 = del2.sum(1).reshape(-1, 1)
 
         w1 -= alpha * gradw1
         w2 -= alpha * gradw2
         b1 -= alpha * gradb1
         b2 -= alpha * gradb2
 
-plt.plot(losses)
-plt.show()
+    plt.plot(losses)
+    plt.show()
+
+if __name__ == '__main__':
+    run(w1, w2, b1, b2)
